16e7706b1db48c8a77860c919c9535b322853809
lucasmezen972-ui <lucas.mezen.972@gmail.com>
2026-07-17 17:13:44 -0400

    test: remove personal username from telemetry fixture

--- Tests/MixPilotCoreTests/CloudObservabilityTests.swift
+++ Tests/MixPilotCoreTests/CloudObservabilityTests.swift
@@ -10,7 +10,7 @@ struct CloudObservabilityTests {
             payload: [
                 "track_title": "Secret Song",
                 "artist": "Secret Artist",
-                "file_path": "/Users/lucas/Music/file.mp3",
+                "file_path": "/Users/example/Music/file.mp3",
                 "command": "PlayPause",
                 "latency_ms": "42"
             ]
